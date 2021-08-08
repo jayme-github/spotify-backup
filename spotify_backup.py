acbbe49229698f3647a3390be97df28bc16d5e09
jayme-github <jayme-github@users.noreply.github.com>
2021-08-08 19:40:46 +0200

    Check for already backed up playlists, add backup_saved_tracks

--- spotify_backup.py
+++ spotify_backup.py
@@ -5,14 +5,14 @@ import argparse
 import logging
 import json
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import CacheFileHandler
 
 
-SCOPE = "playlist-read-private,playlist-read-collaborative"
+SCOPE = "playlist-read-private,playlist-read-collaborative,user-library-read"
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +37,22 @@ class SpotifyBackup:
         )
         return Spotify(auth_manager=auth_manager)
 
+    @staticmethod
+    def _dump_json(path: Path, j: Dict):
+        # Ensure .json suffix
+        if not path.suffix == ".json":
+            path = path.parent / (path.name + ".json")
+        with open(path, "w") as f:
+            json.dump(j, f, sort_keys=True, indent=2)
+
+    def _ensure_dir(self, what: Optional[Path] = None) -> Path:
+        if what is None:
+            bdir = self.backup_path
+        else:
+            bdir = self.backup_path / what
+        bdir.mkdir(parents=True, exist_ok=True)
+        return bdir
+
     def get_playlists(self) -> List[Dict]:
         result = self.sp.current_user_playlists()
         playlists = result["items"]
@@ -47,12 +63,28 @@ class SpotifyBackup:
 
         return playlists
 
-    @staticmethod
-    def _dump_json(path: Path, j: Dict):
-        with open(path, "w") as f:
-            json.dump(j, f, sort_keys=True, indent=2)
+    def backup_everything(self):
+        self.backup_playlists()
+        self.backup_saved_tracks()
 
-    def backup_playlist(self, playlist_id: str, path: Path):
+    def _backup_playlist(self, playlist: Dict, path: Path):
+        """Backup a playlist, including all track details"""
+        # Check if we have a backup already
+        playlist_id = playlist["id"]
+        playlist_path = path / (playlist_id + ".json")
+        # The snapshot_id of spotify playlists changes for every response, so don't
+        # bother checking those.
+        if not playlist["owner"]["id"] == "spotify":
+            try:
+                with open(playlist_path, "r") as f:
+                    have_snapshot_id = json.load(f).get("snapshot_id", None)
+            except:
+                have_snapshot_id = None
+            if playlist["snapshot_id"] == have_snapshot_id:
+                # We already have a up to date backup of this playlist
+                logger.debug('Playlist "%s": Backup is up to date', playlist["name"])
+                return
+
         playlist_result = self.sp.playlist(playlist_id)
         all_items = playlist_result["tracks"]["items"]
         # Read all tracks and extend the tracks array with all of them.
@@ -63,22 +95,31 @@ class SpotifyBackup:
             track_result["items"] = all_items
             playlist_result["tracks"] = track_result
 
-        self._dump_json(path / playlist_id, playlist_result)
+        self._dump_json(playlist_path, playlist_result)
+        logger.info('Playlist "%s": Backup created', playlist["name"])
 
     def backup_playlists(self):
+        """Backup all users playlists (own and starred)"""
         path = Path("playlists")
         for playlist in self.get_playlists():
             if playlist["owner"]["id"] == self.user_id:
                 # my playlist
-                self.backup_playlist(playlist["id"], self.backup_dir(path / "my"))
+                backup_dir = self._ensure_dir(path / "my")
             else:
                 # starred playlist
-                self.backup_playlist(playlist["id"], self.backup_dir(path / "starred"))
+                backup_dir = self._ensure_dir(path / "starred")
+            self._backup_playlist(playlist, backup_dir)
 
-    def backup_dir(self, what: Path) -> Path:
-        bdir = self.backup_path / what
-        bdir.mkdir(parents=True, exist_ok=True)
-        return bdir
+    def backup_saved_tracks(self):
+        """Backup users saved tracks"""
+        result = self.sp.current_user_saved_tracks()
+        saved_tracks = result["items"]
+
+        while result["next"]:
+            result = self.sp.next(result)
+            saved_tracks.extend(result["items"])
+
+        self._dump_json(self._ensure_dir() / "saved_tracks.json", saved_tracks)
 
 
 def main():
@@ -108,7 +149,7 @@ def main():
     )
 
     sb = SpotifyBackup(args.backup_dir)
-    sb.backup_playlists()
+    sb.backup_everything()
 
 
 if __name__ == "__main__":
